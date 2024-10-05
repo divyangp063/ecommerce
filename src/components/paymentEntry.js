f0c2027c8836c710f0571501bf7f79753d12c450
divyangp063 <prajapati.30@buckeyemail.osu.edu>
2024-10-05 13:24:31 -0400

    Purchase complete and partial payment entry complete

--- src/components/paymentEntry.js
+++ src/components/paymentEntry.js
@@ -1,15 +1,18 @@
 import React from "react";
+import {useLocation} from "react-router-dom";
 
-const purchaseEntry = () => {
-  let title = "payment page";
+const PaymentEntry = () => {
+  
+  const location = useLocation();
 
   return (
     <div>
       <h1>
-        {title}
-        </h1>
+        product 1 {location.state.order.buyQuantity[0]}
+        product 2 {location.state.order.buyQuantity[1]}
+      </h1>
     </div>
   );
 };
 
-export default purchaseEntry;
+export default PaymentEntry;
--- src/components/purchase.js
+++ src/components/purchase.js
@@ -1,15 +1,40 @@
-import React from "react";
+import React from "react"
+import {useState} from "react"
+import {useNavigate} from "react-router-dom"
 
-const purchase = () => {
-  let title = "purchase page";
+const Purchase = () => {
+  const [order, setOrder] = useState({
+    buyQuantity: [0,0,0,0,0], credit_card_number: '', expir_date: '', cvvCode: '',
+    card_holder_name: '', address_1: '', address_2: '', city: '', state: '', zip: '',
+  });
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    navigate('/purchase/paymentEntry', {order: order, setOrder: setOrder});}
+
+  console.log('order: ', order);
 
   return (
     <div>
-      <h1>
-        {title}
-        </h1>
+      <form onSubmit={handleSubmit}>
+        <label>Product 1</label>
+        <input 
+          type="number"
+          required
+          onChange={(e) => {order.buyQuantity[0] = e.target.value;}}
+        />
+        <br/>
+        <label>Product 2</label>
+        <input 
+          type="number"
+          required
+          onChange={(e) => {order.buyQuantity[1] = e.target.value;}}
+        />
+        <br/>
+        <button className='button'>Pay</button>
+      </form>
     </div>
   );
 };
 
-export default purchase;
+export default Purchase;
